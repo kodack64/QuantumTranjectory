ef32aadb1dbe476fea4ad3a56a15d8deaf6e101a
kodack64 <kodack64@msn.com>
2014-08-14 09:11:18 +0900

    add ideal dark state calc

--- EigenCalc/main.cpp
+++ EigenCalc/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <conio.h>
 
 using namespace std;
 
@@ -36,6 +37,27 @@ using namespace std;
 
 const int n=10;
 
+
+class EigenSystem{
+public:
+	static bool compnorm;
+	complex<double> value;
+	Eigen::VectorXcd vec;	
+	EigenSystem(Eigen::VectorXcd _vec,complex<double> val){
+		vec= _vec;
+		value=val;
+	}
+	bool operator < (const EigenSystem &b){
+		if(!compnorm)return value.real()<b.value.real();
+		else return norm(value)<norm(b.value);
+	}
+	bool operator > (const EigenSystem &b){
+		if(!compnorm)return value.real()>b.value.real();
+		else return norm(value)>norm(b.value);
+	}
+};
+bool EigenSystem::compnorm = false;
+
 class CalcDiagonal{
 public:
 	double eps;
@@ -45,8 +67,19 @@ public:
 	double gp;
 	double gc;
 	Eigen::MatrixXcd mat;
+	Eigen::MatrixXcd d1mat;
+	Eigen::MatrixXcd d2mat;
+	bool forceG20;
+	CalcDiagonal()
+	:forceG20(false){
+	}
 	void init(){
+		eigen.clear();
+		d1eigen.clear();
+		d2eigen.clear();
 		mat = Eigen::MatrixXcd::Zero(n,n);
+		d1mat = Eigen::MatrixXcd::Zero(3,3);
+		d2mat = Eigen::MatrixXcd::Zero(6,6);
 		mat(mg00,mg00)=0;
 
 		mat(mg10,mg10)=-k1;
@@ -71,13 +104,33 @@ public:
 		mat(mg20,me10) = mat(me10,mg20) = complex<double>(0,sqrt(2)*gp);
 		mat(mf11,me10) = mat(me10,mf11) = complex<double>(0,gc);
 		mat(mf11,mef01) = mat(mef01,mf11) = complex<double>(0,gp);
-//		mat(m2f02,mef01) = mat(mef01,m2f02) = complex<double>(0,sqrt(2)*gc);
-		mat(m2f02,mef01) = mat(mef01,m2f02) = complex<double>(0,2*gc);
+		if(!forceG20)mat(m2f02,mef01) = mat(mef01,m2f02) = complex<double>(0,2*gc);
+		else mat(m2f02,mef01) = mat(mef01,m2f02) = complex<double>(0,sqrt(2)*gc);
 
 		mat(m2e00,me10) = mat(me10,m2e00) = complex<double>(0,sqrt(2)*gp);
 		mat(m2e00,mef01) = mat(mef01,m2e00) = complex<double>(0,sqrt(2)*gc);
+
+		d1mat(0,1)=d1mat(1,0)=complex<double>(0,gp);
+		d1mat(2,1)=d1mat(1,2)=complex<double>(0,gc);
+		d2mat(0,1)=d2mat(1,0)=complex<double>(0,sqrt(2)*gp);
+		d2mat(2,1)=d2mat(1,2)=complex<double>(0,gc);
+		d2mat(2,3)=d2mat(3,2)=complex<double>(0,gp);
+		if(!forceG20)d2mat(4,3)=d2mat(3,4)=complex<double>(0,2*gc);
+		else d2mat(4,3)=d2mat(3,4)=complex<double>(0,sqrt(2)*gc);
+		d2mat(1,5)=d2mat(5,1)=complex<double>(0,sqrt(2)*gp);
+		d2mat(3,5)=d2mat(5,3)=complex<double>(0,sqrt(2)*gc);
+
+//		d2mat(0,0)=-2*k1;
+//		d2mat(1,1)=-k1-r;
+//		d2mat(2,2)=-k1-k2;
+//		d2mat(3,3)=-k2-r;
+//		d2mat(4,4)=-2*k2;
+//		d2mat(5,5)=-2*r;
 	}
 
+	double np;
+	double ne;
+	double nc;
 	double np1;
 	double np2;
 	double nc1;
@@ -110,33 +163,71 @@ public:
 	Eigen::VectorXcd eigenvec;
 	Eigen::VectorXd eigenvecpos;
 	Eigen::VectorXcd eigenval;
+	vector<EigenSystem> eigen,d1eigen,d2eigen;
+	Eigen::VectorXcd d1vec,d2vec;
 	virtual void compute(){
 		Eigen::ComplexEigenSolver<Eigen::MatrixXcd> es(mat);
-		eigenval =  es.eigenvalues();
+		Eigen::ComplexEigenSolver<Eigen::MatrixXcd> d1es(d1mat);
+		Eigen::ComplexEigenSolver<Eigen::MatrixXcd> d2es(d2mat);
 
-		vector<double> reals;
-		for(int i=0;i<n;i++)reals.push_back(eigenval(i).real());
-		int maxind;
-		maxreal=-1e10;
-		for(int i=0;i<n;i++)if(reals[i]>maxreal){
-			maxreal=reals[i];
-			maxind=i;
+		eigenval =  es.eigenvalues();
+		auto darkvec1 =  d1es.eigenvectors();
+		auto darkvec2 =  d2es.eigenvectors();
+		Eigen::VectorXcd d2vecd=Eigen::VectorXcd::Zero(6);
+		d2vecd[0]=gc*gc;
+		d2vecd[2]=-sqrt(2)*gc*gp;
+		d2vecd[4]=gp*gp/sqrt(2);
+		d2vecd.normalize();
+
+		for(int i=0;i<n;i++){
+			eigen.push_back(EigenSystem(es.eigenvectors().col(i),es.eigenvalues()[i]));
 		}
-		sort(reals.rbegin(),reals.rend());
-		double secreal = reals[1];
-		eigenvec = es.eigenvectors().col(maxind);
+		for(int i=0;i<3;i++){
+			d1eigen.push_back(EigenSystem(d1es.eigenvectors().col(i),d1es.eigenvalues()[i]));
+		}
+		for(int i=0;i<6;i++){
+			d2eigen.push_back(EigenSystem(d2es.eigenvectors().col(i),d2es.eigenvalues()[i]));
+		}
+
+//		for(int i=0;i<6;i++) cout << d2eigen[0].vec[i] << " "; cout << endl;
+//		for(int i=0;i<6;i++) cout << d2eigen[1].vec[i] << " "; cout << endl;
+		d2eigen[0].vec=d2vecd,0;
+		d2eigen[1].vec=(d2eigen[1].vec-d2vecd.dot(d2eigen[1].vec)*d2vecd).normalized();
+//		for(int i=0;i<6;i++) cout << d2eigen[0].vec[i] << " "; cout << endl;
+//		for(int i=0;i<6;i++) cout << d2eigen[1].vec[i] << " "; cout << endl;
+//		cout << d2mat*d2eigen[0].vec << endl;
+//		cout << d2mat*d2eigen[1].vec << endl;
+
+		EigenSystem::compnorm=false;
+		sort(eigen.rbegin(),eigen.rend());
+		EigenSystem::compnorm=true;
+		sort(d1eigen.begin(),d1eigen.end());
+		EigenSystem::compnorm=false;
+		sort(d2eigen.rbegin(),d2eigen.rend());
+
+		maxreal = eigen[0].value.real();
+		secreal = eigen[1].value.real();
+
+		eigenvec = eigen[0].vec;
 		eigenvecpos = Eigen::VectorXd(n);
 		for(int i=0;i<n;i++){
 			eigenvecpos(i) = norm(eigenvec(i));
 		}
 
+		//コヒーレンス時間
 		coherence = 1.0/(maxreal-secreal);
+		//mごとの平均光子数
 		np1 = eigenvecpos(mg10);
 		nc1 = eigenvecpos(mf01);
 		ne1 = eigenvecpos(me00);
 		np2 = 2*eigenvecpos(mg20)+eigenvecpos(me10)+eigenvecpos(mf11);
 		nc2 = eigenvecpos(mf11)+eigenvecpos(mef01)+2*eigenvecpos(m2f02);
 		ne2 = eigenvecpos(me10)+eigenvecpos(mef01)+2*eigenvecpos(m2e00);
+		np = np1+np2;
+		ne = ne1+ne2;
+		nc = nc1+nc2;
+
+		//g2
 		g2p=2*eigenvecpos(mg20)/pow(eigenvecpos(mg10),2);
 		g2c=2*eigenvecpos(m2f02)/pow(eigenvecpos(mf01),2);
 		g2e=2*eigenvecpos(m2e00)/pow(eigenvecpos(me00),2);
@@ -147,6 +238,8 @@ public:
 		pos2 = eigenvecpos(mg20)+eigenvecpos(me10)+eigenvecpos(mf11)+eigenvecpos(mef01)+eigenvecpos(m2f02)+eigenvecpos(m2e00);
 
 		// ダーク状態との重なり
+
+		/*
 		if(gp==0 && gc==0){
 			dark0=pos0;
 			dark1=dark2=0;
@@ -162,8 +255,75 @@ public:
 			darkmatchg2 = (pow(pos1,2)/pos2)*eigenvecpos(mg20)/pow(eigenvecpos(mg10),2);
 			darkmatchg2theory = 1 + 0.5*pow(gp,4)/(pow(gc,4)+2*pow(gp*gc,2)+0.5*pow(gp,4));
 		}
-		energyg2 = 2*pos2/pow(pos1,2);
+		*/
 
+		d1vec = Eigen::VectorXcd::Zero(3);
+		d2vec = Eigen::VectorXcd::Zero(6);
+		d1vec[0] = eigenvec(mg10);
+		d1vec[1] = eigenvec(me00);
+		d1vec[2] = eigenvec(mf01);
+		d2vec[0] = eigenvec(mg20);
+		d2vec[1] = eigenvec(me10);
+		d2vec[2] = eigenvec(mf11);
+		d2vec[3] = eigenvec(mef01);
+		d2vec[4] = eigenvec(m2f02);
+		d2vec[5] = eigenvec(m2e00);
+		d1vec.normalize();
+		d2vec.normalize();
+
+		dark0=pos0;
+		dark1=darkmatch1 = norm(d1vec.dot(d1eigen[0].vec));
+		dark2=darkmatch2 = norm(d2vec.dot(d2eigen[0].vec));
+
+		energyg2 = 2*pos2/pow(pos1,2);
+		darkmatchg2 = norm(d2vec[0]/pow(d1vec[0],2));
+
+//		cout << gc << endl ;
+/*		cout << "d1*";
+		for(int i=0;i<3;i++){
+			cout << norm(d1vec.dot(d1eigen[i].vec)) << " " ;
+		}
+*/
+		/*
+		//		cout << d2eigen[0].value << " ::: " << d2eigen[0].vec << endl;
+		cout << endl << "d1* diag elements" << endl;
+		for(int i=0;i<3;i++){
+			cout << norm(d1vec[i]) << " ";
+		}
+		cout << endl;
+		cout << endl << "d1* dark diag elements" << endl;
+		for(int i=0;i<3;i++){
+			cout << norm(d1eigen[0].vec[i]) << " ";
+		}
+		cout << endl;
+		cout << endl << "d2* diag elements" << endl;
+		for(int i=0;i<6;i++){
+			cout << norm(d2vec[i]) << " ";
+		}
+		cout << endl;
+		cout << endl << "d2* dark diag elements" << endl;
+		for(int i=0;i<6;i++){
+			cout << norm(d2eigen[0].vec[i]) << " ";
+		}
+		cout << endl;
+		cout << endl << "d2* inner products" << endl;
+		for(int i=0;i<6;i++){
+			cout << norm(d2vec.dot(d2eigen[i].vec)) << " " ;
+		}
+		cout << endl;
+		cout << "g2:" << darkmatchg2 << endl << endl;
+//		cout << d2mat << endl;
+//		cout << endl << "d2* dark eigen values" << endl;
+		for(int i=0;i<6;i++){
+//			cout << d2eigen[i].value  << " " ;
+		}
+		cout << endl << "d2* second dark" << endl;
+		for(int i=0;i<6;i++){
+			cout << norm(d2eigen[1].vec[i])  << " " ;
+		}
+		cout << endl;
+		cout << endl;
+		*/
 		// 透過率
 		double etap = gp*gp/k1/r;
 		double etac = gc*gc/k2/r;
@@ -245,6 +405,10 @@ int main(){
 	cd->gp=gp*sqrt(N);
 	cd->gc=gc;
 
+//	cd->init();
+//	cd->compute();
+
+
 	fstream fout,fout2,fout3,fout4;
 	vector<fstream> fsv;
 
@@ -318,9 +482,9 @@ int main(){
 	}
 	fout.close();cd->gc=gc;cd->gp=gp;
 	*/
-	fout.open("darkmatchcheck.txt",ios::out);
-	for(int i=0;i<10000;i++){
-		cd->gc=i*0.001;
+/*	fout.open("darkmatchcheck.txt",ios::out);
+	for(int i=0;i<1000;i++){
+		cd->gc=i*0.01;
 		cd->init();
 		cd->compute();
 		fout << cd->gc << " " 
@@ -338,10 +502,35 @@ int main(){
 			<< cd->np2 << " "
 			<< cd->nc2 << " "
 			<< cd->ne2 << " "
+			<< cd->darkmatch1 << " "
+			<< cd->darkmatch2 << " "
+			<<endl;
+		_getch();
+	}
+	fout.close();cd->gc=gc;cd->gp=gp;
+*/
+	fout.open("elements.txt",ios::out);
+	for(int i=0;i<3000;i++){
+		cd->gc=i*0.001;
+		cd->init();
+		cd->compute();
+		fout << cd->gc << " " 
+			<< norm(cd->d1vec[0]) << " "
+			<< norm(cd->d1vec[1]) << " "
+			<< norm(cd->d1vec[2]) << " "
+			<< norm(cd->d2vec[0]) << " "
+			<< norm(cd->d2vec[1]) << " "
+			<< norm(cd->d2vec[2]) << " "
+			<< norm(cd->d2vec[3]) << " "
+			<< norm(cd->d2vec[4]) << " "
+			<< norm(cd->d2vec[5]) << " "
+			<< cd->darkmatchg2 << " "
 			<<endl;
 	}
 	fout.close();cd->gc=gc;cd->gp=gp;
 
+
+	
 /*	fout.open("sc.txt",ios::out);
 	for(int i=0;i<3000;i++){
 		cd->gc=sqrt(i*0.01*cd->r*cd->k2);
